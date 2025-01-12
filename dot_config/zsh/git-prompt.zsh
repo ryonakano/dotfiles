d488f3beb0bfed728fb22e4fcc076ff8b1706b41
Ryo Nakano <ryonakaknock3@gmail.com>
2025-01-12 16:30:34 +0900

    Add Makefile

--- dot_config/zsh/git-prompt.zsh
+++ dot_config/zsh/git-prompt.zsh
@@ -5,7 +5,7 @@ if [ -f $GIT_PROMPT ]; then
 	GIT_PS1_SHOWUNTRACKEDFILES=true
 	GIT_PS1_SHOWSTASHSTATE=true
 	GIT_PS1_SHOWUPSTREAM=auto
-	
+
 	# c.f. https://zenn.dev/sprout2000/articles/bd1fac2f3f83bc#5.-プロンプトへ色を付ける
 	setopt PROMPT_SUBST; PS1='[%F{002}%n@%m%f %F{123}%~%f%F{212}$(__git_ps1 " (%s)")%f]\$ '
 fi
