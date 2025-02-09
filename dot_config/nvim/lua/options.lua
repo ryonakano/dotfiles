29d673f89c6a17f74a1dd6214342704486e2f8e0
Ryo Nakano <ryonakaknock3@gmail.com>
2025-02-09 22:27:44 +0900

    nvim: Configure terminal

--- dot_config/nvim/lua/options.lua
+++ dot_config/nvim/lua/options.lua
@@ -1,3 +1,6 @@
+local map = vim.keymap.set
+local autocmd = vim.api.nvim_create_autocmd
+
 local options = {
   number = true,
   termguicolors = true, -- enable 24-bit color
@@ -16,3 +19,22 @@ for k, v in pairs(options) do
 end
 
 vim.cmd("colorscheme nightfox")
+
+--------------------------------------------------------------------------------
+-- Configure Terminal
+--------------------------------------------------------------------------------
+map("n", "<Leader>t", "<cmd>belowright new<CR><cmd>terminal<CR>")
+map("t", [[<C-\>]], [[<C-\><C-n>]])
+
+-- Make it possible to start typing soon when the terminal opens
+autocmd({ "TermOpen" }, {
+  command = "startinsert",
+})
+-- Line numbers are not suitable for terminals
+autocmd({ "TermOpen" }, {
+  command = "setlocal nonumber",
+})
+
+autocmd({ "TermOpen" }, {
+  command = "resize 10",
+})
