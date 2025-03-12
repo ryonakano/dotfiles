c3fda35f97f2ded3e92d980e1080552ddb11a6b5
Ryo Nakano <ryonakaknock3@gmail.com>
2025-03-12 22:10:51 +0900

    nvim: F3 to toggle relative number

--- dot_config/nvim/lua/options.lua
+++ dot_config/nvim/lua/options.lua
@@ -23,6 +23,10 @@ vim.g.mapleader = " "
 
 map({"n", "i"}, "<F1>", "<Nop>")
 
+-- Toggle relative number. Originally from https://cohama.hateblo.jp/entry/2013/10/07/020453
+-- C-U to remove all characters between the cursor position and the beginning of the line; see ":help c-CTRL-U"
+map("n", "<F3>", ":<C-u>setlocal relativenumber!<CR>")
+
 --------------------------------------------------------------------------------
 -- Configure Terminal
 --------------------------------------------------------------------------------
