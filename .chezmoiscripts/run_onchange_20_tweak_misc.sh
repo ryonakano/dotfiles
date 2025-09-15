5620276ba373ff14fd6280773581600d7e84fae1
Ryo Nakano <ryonakaknock3@gmail.com>
2025-09-15 02:26:05 +0000

    fix: tweak: Check if virt-manager is installed before customizing

--- .chezmoiscripts/run_onchange_20_tweak_misc.sh
+++ .chezmoiscripts/run_onchange_20_tweak_misc.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-# Give permission to /dev/kvm
-sudo usermod -aG kvm "$USER"
-# Disable polkit authorization
-sudo usermod -aG libvirt "$USER"
+which virt-manager > /dev/null
+if [ $? -eq 0 ]; then
+	# Give permission to /dev/kvm
+	sudo usermod -aG kvm "$USER"
+	# Disable polkit authorization
+	sudo usermod -aG libvirt "$USER"
+fi
