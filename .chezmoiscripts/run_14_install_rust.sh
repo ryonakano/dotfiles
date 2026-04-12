5323badb9287128e479000cebab57ba60040a9ae
Ryo Nakano <ryonakaknock3@gmail.com>
2026-04-12 20:38:39 +0900

    Review

--- .chezmoiscripts/run_14_install_rust.sh
+++ .chezmoiscripts/run_14_install_rust.sh
@@ -2,6 +2,7 @@
 
 # Install rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+. "$HOME/.cargo/env"
 
 # Install cargo-bininstall
 curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
