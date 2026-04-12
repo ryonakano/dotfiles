b67c64fa67694797694cb98446a47e7d5861aad5
Ryo Nakano <ryonakaknock3@gmail.com>
2026-04-12 16:12:17 +0900

    fix: bash: Force install sheldon

--- .chezmoiscripts/run_onchange_22_postinst_bash.sh
+++ .chezmoiscripts/run_onchange_22_postinst_bash.sh
@@ -14,4 +14,4 @@ fi
 
 # Install sheldon
 curl --proto '=https' -fLsS https://rossmacarthur.github.io/install/crate.sh \
-	| bash -s -- --repo rossmacarthur/sheldon --to ~/.local/bin
+	| bash -s -- -f --repo rossmacarthur/sheldon --to ~/.local/bin
