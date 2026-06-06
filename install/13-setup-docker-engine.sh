790c6f66367bf15272070fe4fd4f67fd5f1d0c15
Ryo Nakano <ryonakaknock3@gmail.com>
2026-06-07 01:00:12 +0900

    docker: No rootless mode (#22)

--- install/13-setup-docker-engine.sh
+++ install/13-setup-docker-engine.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-################################################################################
 # Install Docker Engine
 # See https://docs.docker.com/engine/install/ubuntu/#install-using-the-convenience-script
-################################################################################
-# Do not enable autostart in root mode
-curl -fsSL https://get.docker.com | sudo sh -s -- --no-autostart
+curl -fsSL https://get.docker.com | sudo sh
 
-################################################################################
-# Use rootless mode
-# See https://docs.docker.com/engine/security/rootless/
-################################################################################
-dockerd-rootless-setuptool.sh install
+# Allow running docker command without sudo
+sudo usermod -aG docker $USER
--- install/fedora/13-setup-docker-engine.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-################################################################################
-# Setup prerequisites for rootless mode
-# See https://docs.docker.com/engine/security/rootless/#prerequisites
-################################################################################
-# Fedora meets prerequisites by default, so NOP
